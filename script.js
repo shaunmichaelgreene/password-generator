4565f2f2aeb6b5fd27f535c28e3c31313600eb18
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-04-17 12:16:18 -0400

    user input validation added

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function generatePassword() {
   var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //26 characters
   var numericalArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //10 characters
   var specialArray = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', '{', ']', '}', ';', '"', ':', '/', '?', ',', '<', '.', '>']; //29 characters
-  console.log(specialArray.length);
   
   var passwordLength = window.prompt("How many characters long do you want your new password to be? (Please Enter a number between 8 - 128)");
     if (passwordLength === "" || passwordLength === null) { // validation to ensure user does not enter a blank response
@@ -23,27 +22,36 @@ function generatePassword() {
     }
   
 
-  var lowerCase = window.prompt("Would you like your new password to include LOWERCASE characters ('a, b, c, etc...')?");
-    if (lowerCase = true) {
+  var lowerCase = window.confirm("Would you like your new password to include LOWERCASE characters ('a, b, c, etc...')?");
+    if (lowerCase) {
       alert("Your new password WILL contain LOWERCASE characters.");
     }
     else {
       alert("Your new password WILL NOT contain LOWERCASE characters.");
     }
     console.log ("Include lowercase characters in new password: " + lowerCase);
-  var upperCase = window.prompt("Would you like your new password to include UPPERCASE characters ('A, B, C, etc...')?");
-    if (upperCase = true) {
+  var upperCase = window.confirm("Would you like your new password to include UPPERCASE characters ('A, B, C, etc...')?");
+    if (upperCase) {
       alert("Your new password WILL contain UPPERCASE characters.");
     }
+    else {
+      alert("Your new password WILL NOT contain UPPERCASE characters.");
+    }
     console.log ("Include uppercase characters in new password: " + upperCase);
-  var numerical = window.prompt("Would you like your new password to include NUMERICAL characters ('1, 2, 3, etc...')?");
-    if (numerical = true) {
+  var numerical = window.confirm("Would you like your new password to include NUMERICAL characters ('1, 2, 3, etc...')?");
+    if (numerical) {
       alert("Your new password WILL contain NUMERICAL characters.");
     }
+    else {
+      alert("Your new password WILL NOT contain NUMERICAL characters.");
+    }
     console.log ("Include numerical characters in new password: " + numerical);
-  var special = window.prompt("Would you like your new password to include SPECIAL characters ('#, &, %, etc...')?");
-    if (special = true) {
+  var special = window.confirm("Would you like your new password to include SPECIAL characters ('#, &, %, etc...')?");
+    if (special) {
       alert("Your new password WILL contain SPECIAL characters.");
+    }
+    else {
+      alert("Your new password WILL NOT contain SPECIAL characters.");
     }
     console.log ("Include special characters in new password: " + special);
 }  //end of generatePassword() function
